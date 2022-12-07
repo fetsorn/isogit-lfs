672b5e455a4828a9780cccf85717e7de1bf4ac02
Nulo <git@nulo.in>
2022-12-07 20:45:04 -0300

    Export uploadBlob

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { default as downloadBlobFromPointer } from './download';
-export { default as populateCache } from './populateCache';
-export { readPointer } from './pointers';
+export { default as downloadBlobFromPointer } from "./download";
+export { default as populateCache } from "./populateCache";
+export { readPointer } from "./pointers";
+export { default as uploadBlob } from "./upload";
+
